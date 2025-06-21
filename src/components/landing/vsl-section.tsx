1a5478e04b0f93936e87b7c0a8358141391cc07c
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-06-21 14:20:44 +0000

    troque o video por esse https://d3s1jrfpp0f48y.cloudfront.net/k3skl7%2F

--- src/components/landing/vsl-section.tsx
+++ src/components/landing/vsl-section.tsx
@@ -2,13 +2,14 @@ export function VSLSection() {
   return (
     <section className="mb-12 md:mb-20">
       <div className="aspect-video overflow-hidden rounded-lg shadow-2xl">
-        <iframe
-          id="js_video_iframe"
-          src="https://jumpshare.com/embed/jlQnaieHvAHRbTgPHvLE?autoplay=1&muted=1"
-          allow="autoplay; fullscreen"
-          allowFullScreen
-          className="h-full w-full border-0"
-        ></iframe>
+        <video
+          src="https://d3s1jrfpp0f48y.cloudfront.net/k3skl7%2Ffile%2Fd7b16a9ef5c155558a07bf3cf02a8f44_0a7d1df4ce1b6a0ee4d801525406d2a8.mp4?response-content-disposition=inline%3Bfilename%3D%22d7b16a9ef5c155558a07bf3cf02a8f44_0a7d1df4ce1b6a0ee4d801525406d2a8.mp4%22%3B&response-content-type=video%2Fmp4&Expires=1750537178&Signature=Zlm9kIqG2pVQG759chJijnn~oYePWjaa2NHlTn4LFYl2FrHX0sKiKpVFpz0d-yOd6w2zXJ3lyOd~mFQ-aDcfWmr4ZOduA8SuoV5LF30r~W1u~hb4bkZx9aP3XkplRmVWypAQ0b2XP9FsLxtwc-A5Cch7xXv6ElDty8vACTuW1gWXTN~8a5rCgFpIT9yYhJhPpZSF1o2EqGLOr2ALA~g4e2iBPoAcUho61DJrYR2LZQ3EopGYTM~JXYTUNeisUZeOCTGmatjdptDRsHNjvUtCBfjBnTPrqwdZpRCzeFTNktiigHvzKyJNujLWu~atVCzOMkn1JqOE8ppkPnxgTbfyNA__&Key-Pair-Id=APKAJT5WQLLEOADKLHBQ"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="h-full w-full object-cover"
+        />
       </div>
     </section>
   );

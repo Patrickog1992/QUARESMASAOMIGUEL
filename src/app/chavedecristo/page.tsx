3e9b8e4925be7453ea241a7aaa58f0b9b11adcbe
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-01 14:15:02 +0000

    Do jeito que a página está agora eu quero somente que troque a fonte Pop

--- src/app/chavedecristo/page.tsx
+++ src/app/chavedecristo/page.tsx
@@ -134,7 +134,7 @@ export default function ChaveDeCristoPage() {
     }, []);
 
     return (
-        <div className="bg-white text-black font-['Open_Sans',_sans-serif]">
+        <div className="bg-white text-black">
             <StaticTopBanner />
 
             <main className="container mx-auto px-4 py-8 max-w-4xl">

a291e2cb6ee021510ded1ca6b06150345cb61170
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-05 19:24:21 +0000

    troque o video por esse
    <vturb-smartplayer id="vid-68925811e69e8e799c2

--- src/components/landing/vsl-section-arcanjomiguel.tsx
+++ src/components/landing/vsl-section-arcanjomiguel.tsx
@@ -6,17 +6,17 @@ import { BuyButtonArcanjoMiguel } from './buy-button-arcanjomiguel';
 
 export function VSLSectionArcanjoMiguel() {
   useEffect(() => {
-    const scriptId = 'vid-688f4a444c79100934d397b8-script';
+    const scriptId = 'vid-68925811e69e8e799c274811-script';
     if (!document.getElementById(scriptId)) {
         const script = document.createElement('script');
         script.id = scriptId;
-        script.src = 'https://scripts.converteai.net/0d57a35b-ffe2-4eb5-bd3e-291413c756c0/players/688f4a444c79100934d397b8/v4/player.js';
+        script.src = 'https://scripts.converteai.net/1b791434-3e0f-4e75-a646-c8074375e059/players/68925811e69e8e799c274811/v4/player.js';
         script.async = true;
         document.head.appendChild(script);
     }
   }, []);
   
-  const videoHtml = `<vturb-smartplayer id="vid-688f4a444c79100934d397b8" style="display: block; margin: 0 auto; width: 100%; max-width: 400px;"></vturb-smartplayer>`;
+  const videoHtml = `<vturb-smartplayer id="vid-68925811e69e8e799c274811" style="display: block; margin: 0 auto; width: 100%; max-width: 400px;"></vturb-smartplayer>`;
 
   return (
     <section className="mb-12 md:mb-20">

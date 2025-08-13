53afc60e7a37e5c94184414a3822e7ab1cdf7cc8
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-13 16:26:05 +0000

    Suas orações serão sempre ignoradas por Deus se você não recitar as suas

--- src/components/landing/hero-section-segredovaticano.tsx
+++ src/components/landing/hero-section-segredovaticano.tsx
@@ -4,7 +4,7 @@ import { Check, ScrollText } from 'lucide-react';
 export function HeroSectionSegredoVaticano() {
   return (
     <section className="flex flex-col items-center text-center mb-12 md:mb-20">
-      <Card className="bg-accent text-accent-foreground p-4 md:p-6 rounded-xl shadow-lg mb-8 max-w-4xl w-full">
+      <Card className="bg-blue-800 text-white p-4 md:p-6 rounded-xl shadow-lg mb-8 max-w-4xl w-full">
         <h1 className="font-headline text-xl sm:text-2xl md:text-3xl font-bold uppercase leading-tight">
           Suas orações serão sempre ignoradas por Deus se você não recitar as suas ORAÇÕES ORIGINAIS! Somente essas 12 orações são capazes de trazer dinheiro, amor e abundância para sua vida sem esforço
         </h1>

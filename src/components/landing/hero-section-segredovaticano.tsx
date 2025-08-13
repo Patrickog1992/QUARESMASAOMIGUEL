eeb9734bdab24168f88d68fbff0fc7c0ff2fb739
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-13 16:28:12 +0000

    ORAÇÕES ORIGINAIS essa parte também

--- src/components/landing/hero-section-segredovaticano.tsx
+++ src/components/landing/hero-section-segredovaticano.tsx
@@ -6,7 +6,7 @@ export function HeroSectionSegredoVaticano() {
     <section className="flex flex-col items-center text-center mb-12 md:mb-20">
       <Card className="bg-blue-800 text-white p-4 md:p-6 rounded-xl shadow-lg mb-8 max-w-4xl w-full">
         <h1 className="font-headline text-xl sm:text-2xl md:text-3xl font-bold uppercase leading-tight">
-          Suas orações serão sempre ignoradas por Deus se você não recitar as suas ORAÇÕES ORIGINAIS! <span className="text-amber-300">Somente essas 12 orações são capazes de trazer dinheiro, amor e abundância</span> para sua vida sem esforço
+          Suas orações serão sempre ignoradas por Deus se você não recitar as suas <span className="text-amber-300">ORAÇÕES ORIGINAIS</span>! <span className="text-amber-300">Somente essas 12 orações são capazes de trazer dinheiro, amor e abundância</span> para sua vida sem esforço
         </h1>
       </Card>
 

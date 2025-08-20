f51eddb4d8bf517eb8bf7f2ebc6d1cf0a943e7fa
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-20 18:46:56 +0000

    no final da páginaestá escrito segredo do vaticano

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
           <Link href="#" className="hover:text-foreground transition-colors">Política de Privacidade</Link>
         </div>
         <p>
-          © 2025 O Segredo do Vaticano. Todos os direitos reservados.
+          © 2025 Sabedoria de Salomão. Todos os direitos reservados.
         </p>
         <p className="max-w-3xl mx-auto">
           Este site não é afiliado ao Facebook ou a qualquer entidade do Facebook. Depois que você sair do Facebook, a responsabilidade não é deles e sim do nosso site. Fazemos todos os esforços para indicar claramente e mostrar todas as provas do produto e usamos resultados reais. Nós não vendemos o seu e-mail ou qualquer informação para terceiros. Jamais fazemos qualquer tipo de spam. Se você tiver alguma dúvida, sinta-se à vontade para usar o link de contato e falar conosco em horário comercial de Segunda a Sextas das 09h00 às 18h00. Lemos e respondemos todas as mensagens por ordem de chegada.

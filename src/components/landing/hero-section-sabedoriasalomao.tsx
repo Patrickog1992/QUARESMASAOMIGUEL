47902a6edcee982371687c38414e1b12be942e7e
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-20 18:36:30 +0000

    diminua a headline para caber em 4 linhas

--- src/components/landing/hero-section-sabedoriasalomao.tsx
+++ src/components/landing/hero-section-sabedoriasalomao.tsx
@@ -3,7 +3,7 @@ import { Check } from 'lucide-react';
 export function HeroSectionSabedoriaSalomao() {
   return (
     <section className="space-y-4">
-      <h1 className="text-3xl md:text-5xl font-bold uppercase text-amber-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]">
+      <h1 className="text-2xl md:text-4xl font-bold uppercase text-amber-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]">
         A ORAÇÃO PERDIDA DE SALOMÃO é capaz de prover milagres financeiros e a restauração da sáude em até 7 dias
       </h1>
        <p className="font-bold text-amber-300 uppercase tracking-wider animate-pulse md:text-lg">

5fd2b013f6675f7c12ee005120927be4e7cd5142
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-23 02:41:38 +0000

    troque para 51 minutos e 57 segundos

--- src/app/anjo/video/page.tsx
+++ src/app/anjo/video/page.tsx
@@ -108,7 +108,7 @@ const CheckoutButton = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowButton(true);
-        }, (51 * 60 + 15) * 1000); // 51 minutes and 15 seconds
+        }, (51 * 60 + 57) * 1000); // 51 minutes and 57 seconds
         
         return () => clearTimeout(timer);
     }, []);

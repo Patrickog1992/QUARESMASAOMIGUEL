6903fe05cb2b075418f49bf54a1a67c8abc67560
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-23 03:19:00 +0000

    mostre o botão de checkout com 5 segundos

--- src/app/anjo/video/page.tsx
+++ src/app/anjo/video/page.tsx
@@ -108,7 +108,7 @@ const CheckoutButton = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowButton(true);
-        }, (51 * 60 + 57) * 1000); // 51 minutes and 57 seconds
+        }, 5 * 1000); // 5 seconds
         
         return () => clearTimeout(timer);
     }, []);

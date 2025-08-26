13f1562f4749c734746b72fdc59dfbbe316fc48a
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-08-26 17:13:02 +0000

    mostre o botão de checkout com 5 segundos para ver se está tudo certo

--- src/app/chavedomilagre/video/page.tsx
+++ src/app/chavedomilagre/video/page.tsx
@@ -149,7 +149,7 @@ const CheckoutButton = () => {
     useEffect(() => {
         const timer = setTimeout(() => {
             setShowButton(true);
-        }, (9 * 60 + 22) * 1000); // 9 minutes and 22 seconds
+        }, 5 * 1000); // 5 seconds
 
         return () => clearTimeout(timer);
     }, []);

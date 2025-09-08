95b3fe53f6b79b79c8d6791cf6de1dce0cd5461a
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-08 18:03:45 +0000

    troque o video por
    <vturb-smartplayer id="vid-68bedf9ec93859a0d8db43e8

--- src/app/anjo/video/page.tsx
+++ src/app/anjo/video/page.tsx
@@ -82,17 +82,17 @@ const comments = [
 
 const VideoPlayer = () => {
     useEffect(() => {
-        const scriptId = 'vturb-player-script-68a624a3bd124806b33557a8';
+        const scriptId = 'vturb-player-script-68bedf9ec93859a0d8db43e8';
         if (document.getElementById(scriptId)) return;
 
         const script = document.createElement('script');
         script.id = scriptId;
-        script.src = "https://scripts.converteai.net/eaf579c8-6aa1-4f6f-b5bd-8ba46f9e23f8/players/68a624a3bd124806b33557a8/v4/player.js",
+        script.src = "https://scripts.converteai.net/1a6a90ad-f1f6-4f11-b6be-e02b59de709c/players/68bedf9ec93859a0d8db43e8/v4/player.js",
         script.async = true;
         document.head.appendChild(script);
     }, []);
 
-    const videoHtml = `<vturb-smartplayer id="vid-68a624a3bd124806b33557a8" style="display: block; margin: 0 auto; width: 100%; max-width: 400px;"></vturb-smartplayer>`;
+    const videoHtml = `<vturb-smartplayer id="vid-68bedf9ec93859a0d8db43e8" style="display: block; margin: 0 auto; width: 100%; max-width: 400px;"></vturb-smartplayer>`;
 
     return (
         <div 

02d4b57cf0b65c85c54ecc07166ecbf3f0186335
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-16 13:39:30 +0000

    ATENÇÃO : Devido aos grandes ricos do nosso país estar tentando nos proc

--- src/app/reisalomao/page.tsx
+++ src/app/reisalomao/page.tsx
@@ -10,7 +10,7 @@ import { HebrewPattern } from '@/components/landing/hebrew-pattern';
 export default function ReiSalomaoPage() {
   return (
     <div className="dark relative flex flex-col min-h-screen bg-amber-900 text-white overflow-x-hidden">
-      <TopBanner className="bg-amber-600 text-amber-950" text="ATENÇÃO : Devido aos grandes ricos do nosso país estar tentando nos processar por revelar o segredo esse vídeo ficará disponível somente até hoje" />
+      <TopBanner className="bg-amber-600 text-amber-950" text="ATENÇÃO : Devido aos grandes ricos do nosso país estar tentando nos processar por revelar o segredo esse vídeo ficará disponível somente até hoje : 16 de setembro" />
       <HebrewPattern />
       <main className="flex-grow container mx-auto px-4 py-8 md:py-12 z-10">
         <div className="max-w-3xl mx-auto space-y-12 text-center">

dea63fc68905bf5e5715ce0ca803034c9bad2a72
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-19 17:55:56 +0000

    na sub headline coloque
    Ele liberou as orações e mais de 47 mil Brasil

--- src/components/landing/hero-section-arcanjomiguel.tsx
+++ src/components/landing/hero-section-arcanjomiguel.tsx
@@ -8,6 +8,9 @@ export function HeroSectionArcanjoMiguel() {
       <h1 className="text-3xl md:text-5xl font-bold uppercase text-amber-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]">
         Padre REGINALDO MANZOTTI traduziu 50 orações do Arcanjo Miguel que estavam escondidas
       </h1>
+      <p className="text-lg md:text-xl font-medium text-amber-100">
+        Ele liberou as orações e mais de 47 mil Brasileiros já foram abênçoados
+      </p>
        <p className="font-bold text-amber-300 uppercase tracking-wider animate-pulse md:text-lg">
             Assista ao vídeo abaixo e receba a sua bênção
         </p>

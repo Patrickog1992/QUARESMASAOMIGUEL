0d8e1088b817813d118f8a71f3fd2979b4ce35a7
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-19 18:06:00 +0000

    hoje : 19 de setembro
    Padre REGINALDO MANZOTTI traduziu 50 orações secr

--- src/components/landing/hero-section-arcanjomiguel.tsx
+++ src/components/landing/hero-section-arcanjomiguel.tsx
@@ -6,7 +6,7 @@ export function HeroSectionArcanjoMiguel() {
   return (
     <section className="space-y-4">
       <h1 className="text-3xl md:text-5xl font-bold uppercase text-amber-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]">
-        Padre REGINALDO MANZOTTI traduziu 50 orações do Arcanjo Miguel que estavam escondidas
+        Padre REGINALDO MANZOTTI traduziu 50 orações secretas do Arcanjo Miguel que estavam escondidas
       </h1>
       <p className="text-lg md:text-xl font-medium text-amber-100">
         Ele liberou as orações e mais de 47 mil Brasileiros já foram abênçoados

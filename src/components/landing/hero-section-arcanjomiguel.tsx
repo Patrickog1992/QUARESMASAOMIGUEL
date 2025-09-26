d738727aa28beea19e5958d2225002ae5c29d348
Patrick Oliveira <patrickpneus2018@hotmail.com>
2025-09-26 18:12:01 +0000

    AS ORAÇÕES SECRETAS DE SÃO MIGUEL ARCANJO VEM FAZENDO MILAGRES NA VIDA D

--- src/components/landing/hero-section-arcanjomiguel.tsx
+++ src/components/landing/hero-section-arcanjomiguel.tsx
@@ -6,10 +6,10 @@ export function HeroSectionArcanjoMiguel() {
   return (
     <section className="space-y-4">
       <h1 className="text-3xl md:text-5xl font-bold uppercase text-amber-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.7)]">
-        Padre REGINALDO MANZOTTI traduziu 50 orações secretas do Arcanjo Miguel que estavam escondidas
+        AS ORAÇÕES SECRETAS DE SÃO MIGUEL ARCANJO VEM FAZENDO MILAGRES NA VIDA DE MILHARES DE BRASILEIROS
       </h1>
       <p className="text-lg md:text-xl font-medium text-amber-100">
-        AS ORAÇÕES SECRETAS DE SÃO MIGUEL ARCANJO VEM FAZENDO MILAGRES NA VIDA DE MILHARES DE BRASILEIROS
+        Padre REGINALDO MANZOTTI traduziu 50 orações secretas do Arcanjo Miguel que estavam escondidas
       </p>
       <p className="text-md md:text-lg text-amber-100">
         Receba sua benção de saúde, prosperidade e finança
